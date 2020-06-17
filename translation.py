a2ea3e35cafc1ea6a6a731d94b9c5a5c8fd9f968
Abdullah Al Anas Himel <43927831+aaahimel@users.noreply.github.com>
2020-06-17 18:52:04 +0600

    Update translation.py

--- translation.py
+++ translation.py
@@ -16,14 +16,14 @@ source code : <a href='https://github.com/sahadz/convert-2-video'>Click here</a>
     DOWNLOAD_START = "trying to download"
     UPLOAD_START = "trying to upload"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Here is your File Sir👇👇"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "Join @Dx_botz...have a nyc day!"
+    CUSTOM_CAPTION_UL_FILE = "Join @MoviesByte"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
@@ -31,10 +31,8 @@ Telegram ID: <code>{}</code>
 Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
 Expires on: {}"""
     HELP_USER = """I can do:
-👉 <a href="https://telegram.dog/FileConvertRobot">Convert To Streamable video any telegram file</a>
---------
-Send /me to know current plan details
-
-Support Group : @Infotelgroup"""
+👉Convert To Streamable video any telegram file
+----------------------------------
+Send /me to know current plan details"""
 
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
